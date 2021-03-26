fe14e7d9cdfdcb56e8b84c97af628cead806bb65
Gabriel Souza <augustonga@outlook.com>
2021-03-25 23:52:32 -0300

    adição de background + ajustes da função de pesquisa

--- main.js
+++ main.js
@@ -5,8 +5,21 @@ function consultaCep(){
         url: url,
         type: "GET",
         success: function(response){
+            if(response.erro == true){
+                alert("CEP não encontrado")
+            }
             console.log(response);
-            $("#dados").html(response.logradouro)
+            $("#rua").html(response.logradouro)
+            $("#bairro").html(response.bairro)
+            $("#localidade").html(response.localidade)
+            $("#estado").html(response.uf)
+            $(".consultCep").show();
+        },
+        error:  function(response){
+            alert("CEP não encontrado");
         }
     })
 }
+$(function(){
+    $(".consultCep").hide();
+});
